d6271824d494110069002fe5d2ecc5e2e7c06b3c
medvedium <mumbles2606@gmail.com>
2020-05-10 13:32:44 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -9,14 +9,14 @@ function testWebP(callback) {
     callback(webP.height == 2);
   };
   webP.src =
-    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
 }
 
 testWebP(function (support) {
   if (support == true) {
-    document.querySelector("body").classList.add("webp");
+    document.querySelector('body').classList.add('webp');
   } else {
-    document.querySelector("body").classList.add("no-webp");
+    document.querySelector('body').classList.add('no-webp');
   }
 });
 
@@ -24,42 +24,46 @@ let map;
 let myIcon;
 
 DG.then(function () {
-  map = DG.map("map", {
+  map = DG.map('map', {
     center: [59.93864, 30.323],
     zoom: 16,
   });
   myIcon = DG.icon({
-    iconUrl: "img/map-marker.svg",
+    iconUrl: 'img/map-marker.svg',
     iconSize: [52, 68],
   });
   DG.marker([59.93864, 30.323], {
     icon: myIcon,
   })
     .addTo(map)
-    .bindPopup("Санкт-Петербург, ул Большая Конюшенная, д 19");
+    .bindPopup('Санкт-Петербург, ул Большая Конюшенная, д 19');
 });
 
 $(document).ready(function () {
-  $(document).on("click", ".header__nav--list li a", function () {
-    var linkID = $(this).attr("href");
-    $("html, body").animate(
-      {
-        scrollTop: $(linkID).offset().top,
-      },
-      "slow"
-    );
-  });
-  $(document).on("click", ".footer__nav--list li a", function () {
-    var linkID = $(this).attr("href");
-    $("html, body").animate(
-      {
-        scrollTop: $(linkID).offset().top,
-      },
-      "slow"
-    );
+  $('.header__burger').click(function (event) {
+    $('.header__burger, .header__nav--mobile').toggleClass('active');
+    $('body').toggleClass('lock');
   });
 
-  $(".rooms__inner").slick({
+  $(document).on(
+    'click',
+    '.header__nav--link, .footer__nav--link',
+    function () {
+      let linkID = $(this).attr('href');
+      $('html, body').animate(
+        {
+          scrollTop: $(linkID).offset().top,
+        },
+        'slow',
+      );
+      if ($(this).hasClass('header__nav--link')) {
+        $('.header__burger, .header__nav--mobile').toggleClass('active');
+        $('body').toggleClass('lock');
+      }
+    },
+  );
+
+  $('.rooms__inner').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -78,7 +82,7 @@ $(document).ready(function () {
       },
     ],
   });
-  $(".reviews__inner").slick({
+  $('.reviews__inner').slick({
     infinite: true,
     slidesToShow: 2,
     slidesToScroll: 1,
